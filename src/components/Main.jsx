cda899fc82f4e5582d2d1987995491288e052de7
mithun241105 <mithun24112005@gmail.com>
2025-07-30 00:44:04 +0530

    my react project

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useRef, useEffect } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
-import Context from '../context/context'
+import Context from '../context/Context'
 
 function Main() {
     const { onSent, messages, loading, setinput, input } = useContext(Context);
